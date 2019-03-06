52076f77867086cc70729da8b8c320b1581df0e1
Nik Malhotra <nmal@me.com>
2019-03-06 22:21:40 +0000

    exercise complete

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    if (this.transactions.length !== 0) {
+      const reducer = (acc, currVal) => acc + currVal;
+      return this.transactions.reduce(reducer);
 
-class Withdrawal {
+    } else {
+      return 0;
+    }
 
-  constructor(amount) {
-    this.amount = amount;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.account = account;
+    this.amount = amount;
+  }
+  isAllowed() {
+    if ((this.account.balance <= 0) && this.value <= 0) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    if (this.isAllowed()) {
+      this.account.addTransaction(this.value);
+      console.log(`Transaction of $${this.value} complete.`)
+    } else {
+      console.log(`Transaction of $${this.value} declined. Insufficient funds.`)
+    }
+    //this.account.balance += this.value; // value's amount is decided by the sub
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {   // remember, getters and setters can be defined in js as such... by saying get function() and set function()
+    return -this.amount;
+
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
   }
 
 }
@@ -18,12 +65,17 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t0 = new Withdrawal(100, myAccount);
+t0.commit();
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:$', myAccount.balance);
